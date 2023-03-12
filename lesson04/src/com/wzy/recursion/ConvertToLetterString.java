8f1a68053cd39e07a0466095c87c4dd4ea73772f
wzy <wzy@hfut.com>
2023-03-12 23:29:52 +0800

    字符串转换问题改动态规划

--- lesson04/src/com/wzy/recursion/ConvertToLetterString.java
+++ lesson04/src/com/wzy/recursion/ConvertToLetterString.java
@@ -44,6 +44,37 @@ public class ConvertToLetterString {
         return process(str, i + 1);
     }
 
+    public static int dpWay(String s) {
+        if (s == null || s.length() == 0){
+            return 0;
+        }
+        char[] str = s.toCharArray();
+        int N = str.length;
+        int[] dp = new int[N + 1];
+        dp[N] = 1;
+        // 分析暴力递归的过程，dp[i]只和dp[i + 1],dp[i + 2]的位置有关，所以dp表从右向左填
+        // 直接拷贝暴力递归的过程，return的时刻就是dp表修改的时刻
+        for (int i = N - 1; i >= 0; i--){
+            if (str[i] == '0') {
+                dp[i] = 0;
+            }
+            if (str[i] == '1') {
+                dp[i] = dp[i + 1];
+                if (i + 1 < str.length) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+            if (str[i] == '2') {
+                dp[i] = dp[i + 1];
+                if (i + 1 < str.length && (str[i + 1] >= '0' && str[i + 1] <= 6)){
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+
+        return dp[0];
+    }
+
     public static void main(String[] args) {
         System.out.println(number("11"));
     }
