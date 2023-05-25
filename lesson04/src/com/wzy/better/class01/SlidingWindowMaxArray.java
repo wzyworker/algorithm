7d0a5a12a4bb9301d47ad80df8e80237435dc202
wzy <wzy@hfut.com>
2023-05-25 21:55:21 +0800

    滑动窗口最大值问题

---
+++ lesson04/src/com/wzy/better/class01/SlidingWindowMaxArray.java
@@ -0,0 +1,36 @@
+package com.wzy.better.class01;
+
+import java.util.LinkedList;
+
+/**
+ * @author wzy
+ * @date 2023年05月25日 21:45
+ */
+public class SlidingWindowMaxArray {
+
+    public static int[] getMaxWindow(int[] arr, int w){
+        if (arr == null || w < 1 || arr.length < w){
+            return null;
+        }
+        LinkedList<Integer> qmax = new LinkedList<>();
+        int[] res = new int[arr.length - w + 1];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[i]) {
+                qmax.pollLast();
+            }
+            qmax.addLast(i);
+            if (qmax.peekFirst() == i - w) {
+                qmax.pollFirst();
+            }
+            if (i >= w - 1) {
+                res[index++] = arr[qmax.peekFirst()];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
